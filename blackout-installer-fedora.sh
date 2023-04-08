c53a1243387a0e18a9c8a4dbdd1aa03d788b69c6
J.Daniel Cruz <99006002+JDanielDBC@users.noreply.github.com>
2023-04-07 23:40:15 -0600

    Now can be run many times

--- blackout-installer-fedora.sh
+++ blackout-installer-fedora.sh
@@ -22,7 +22,6 @@
 #
 #This is designed for Fedora Linux
 #Este script está diseñado para Fedora Linux
-
 printf "\nWelcome to the Blackout Installer (Fedora Edition)\n"
 sleep 3
 clear
@@ -68,7 +67,18 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 sleep 3
 printf "\nImproving  the dnf config, installing repos and pkgs:\n"
 
-sudo printf "\n#I added for speed\nfastestmirror=True\nmax_parallel_downloads=4\ndefaultyes=True\nkeepcache=True\n" >> /etc/dnf/dnf.conf
+sudo printf "\n[main]
+gpgcheck=True
+installonly_limit=3
+clean_requirements_on_remove=True
+best=False
+skip_if_unavailable=True
+#I added for speed
+fastestmirror=True
+max_parallel_downloads=4
+defaultyes=True
+keepcache=True
+\n" > /etc/dnf/dnf.conf
 
 #/etc/dnf/dnf.conf
 #[main]
@@ -85,16 +95,16 @@ sudo printf "\n#I added for speed\nfastestmirror=True\nmax_parallel_downloads=4\
 #keepcache=True
 
 #For ascii art on terminal:
+git clone https://github.com/JDanielDBC/Blackout-Linux-Logo.git ~/Pictures/blackout-logo
 wget https://github.com/TheZoraiz/ascii-image-converter/releases/download/v1.13.1/ascii-image-converter_Linux_amd64_64bit.tar.gz
 tar xzf ascii-image-converter_Linux_amd64_64bit.tar.gz
 cd ascii-image-converter_Linux_amd64_64bit
 sudo cp ascii-image-converter /usr/local/bin/
-cd ~/
+
 
 #Gtk4 looking gtk3 theme 
 wget https://github.com/lassekongo83/adw-gtk3/releases/download/v4.2/adw-gtk3v4-2.tar.xz
 sudo tar -xf adw-gtk3v4-2.tar.xz -C /usr/share/themes
-gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 
 #Gtk4 Firefox
 curl -s -o- https://raw.githubusercontent.com/rafaelmardojai/firefox-gnome-theme/master/scripts/install-by-curl.sh | bash
@@ -148,12 +158,29 @@ sudo ./dnf.sh
 
 sleep 3
 printf "\nInstalling some Flatpaks:\n"
+flatpak install org.gtk.Gtk3theme.adw-gtk3 org.gtk.Gtk3theme.adw-gtk3-dark -y
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark' && gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 flatpak install flathub com.github.tchx84.Flatseal -y
 flatpak install com.github.tenderowl.frog -y
 flatpak install org.onlyoffice.desktopeditors -y
+flatpak install com.belmoussaoui.Decoder -y
+flatpak install com.github.GradienceTeam.Gradience -y
+flatpak install com.github.fabiocolacio.marker -y
+flatpak install com.github.flxzt.rnote -y
+flatpak install com.github.taiko2k.avvie -y
+flatpak install com.mattjakeman.ExtensionManager -y
+flatpak install dev.geopjr.Collision -y
+flatpak install fr.romainvigier.MetadataCleaner -y
+flatpak install io.github.dubstar_04.design -y
+flatpak install io.github.fsobolev.TimeSwitch -y
+flatpak install io.github.realmazharhussain.GdmSettings -y
+flatpak install io.github.seadve.Mousai -y
+flatpak install io.gitlab.theevilskeleton.Upscaler -y
+flatpak install io.gitlab.adhami3310.Converter -y
+flatpak install org.gustavoperedo.FontDownloader -y
 
 #For Electronic Engineering
-flatpak install cc.arduino.IDE2 -y
+flatpak install cc.arduino.arduinoide -y
 flatpak install com.github.alexhuntley.Plots -y
 flatpak install io.github.ferraridamiano.ConverterNOW -y
 flatpak install org.librepcb.LibrePCB -y
@@ -165,8 +192,7 @@ cd ~/
 
 #Bashrc
 git clone https://github.com/JDanielDBC/Blackout-Linux-Bashrc.git
-rm ~/.bashrc
-cp ./JDaniel-Bashrc/.bashrc ~/
+mv ./JDaniel-Bashrc/.bashrc ~/
 
 printf "\nFinished installation\n"
 
